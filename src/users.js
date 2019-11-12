d22b0ad184483e23d20840eff4ff8825f7ba4c52
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-12 16:24:01 +0100

    users

--- src/users.js
+++ src/users.js
@@ -76,7 +76,7 @@ async function createUser(req, res) {
     const { rows } = await db.query(createQuery, values);
     const token = Helper.generateToken(rows[0].id);
     // console.log(`this is the token ${token}`);
-    return res.status(201).send([{ status: 'success', rows }]);
+    return res.status(201).send(rows);
   } catch (error) {
     if (error.routine === '_bt_check_unique') {
       return res.status(400).send({ message: 'User with that username already exist' });
@@ -120,15 +120,15 @@ async function login(req, res) {
     if (!Helper.comparePassword(rows[0].pword, req.body.password)) {
       return res.status(400).send({ message: 'The credentials you provided is incorrect' });
     }
-    // const token1 = Helper.generateToken(rows[0].id);
+    const token = Helper.generateToken(rows[0].id);
     const data = {
       status: 'success',
       data: {
-        token: token1,
+        token: token,
         userId: rows[0].id,
       },
     };
-    return res.status(200).send({ status: 'success' });
+    return res.status(200).send(token);
   } catch (error) {
     return res.status(400).send(error);
   }
